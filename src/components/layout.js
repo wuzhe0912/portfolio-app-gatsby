f83030f0eb1911cfb851c551444f4d2191db3b96
zhethon <kgb00128@gmail.com>
2019-04-08 23:53:40 +0800

    add sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
+// import '../sass/layout.scss'
 
 const Layout = ({ children }) => (
   <StaticQuery
