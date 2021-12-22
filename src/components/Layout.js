f405d2667d8e55aedc551cd2b60cf7c85f3ec027
Pitt Wu <kgb00128@gmail.com>
2021-12-22 17:29:36 +0800

    feat: add nav

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,17 @@
-import React from "react"
-import Navbar from "./Navbar"
-import Sidebar from "./Sidebar"
-import Footer from "./Footer"
+import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
 
-import "../assets/css/main.css"
+import "../assets/css/main.css";
 const Layout = ({ children }) => {
-  return <>{children}</>
-}
+  return (
+    <>
+      <Navbar></Navbar>
+      {children}
+      <Footer></Footer>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,31 @@
-import React from "react"
-import logo from "../assets/images/logo.svg"
-import { FaAlignRight } from "react-icons/fa"
-import pageLinks from "../constants/links"
-import { Link } from "gatsby"
-const Navbar = () => {
-  return <h2>navbar component</h2>
-}
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+import pageLinks from "../constants/links";
+import { Link } from "gatsby";
 
-export default Navbar
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="Pitt Wu" />
+          <button type="button" className="toggle-btn">
+            <FaAlignRight></FaAlignRight>
+          </button>
+        </div>
+        <div className="nav-links">
+          {pageLinks.map(link => {
+            return (
+              <Link key={link.id} to={link.url}>
+                {link.text}
+              </Link>
+            );
+          })}
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
