99c132c23fc0a537894a22c308822938c2f7c929
Pitt Wu <kgb00128@gmail.com>
2022-02-07 00:20:49 +0800

    fixed: about style

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { graphql } from "gatsby";
-import Title from "../components/Title";
-import Seo from "../components/Seo";
+import React from 'react';
+import { graphql } from 'gatsby';
+import Title from '../components/Title';
+import Seo from '../components/Seo';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const About = () => {
-  const titleText = "About Me";
+  const titleText = 'About Me';
 
   return (
     <section className="section about-page fade-in">
       <Title title={titleText} />
       <div className="row">
         <div className="about-content"></div>
-        <div className="about-img"></div>
+        <div className="about-img-wrpper">
+          <StaticImage
+            src="../assets/images/about.png"
+            className="about-img"
+            alt="about image"
+          />
+        </div>
       </div>
     </section>
   );
